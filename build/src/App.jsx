c8ac3429f779cba2d9b2b2a2ca3874a00a10a54a
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-04-28 21:44:00 +0300

    react

--- build/src/App.jsx
+++ build/src/App.jsx
@@ -2,36 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './main.jsx'
 
 import{ Article, Brand, Cta, Feature, Navbar } from './components'
+import{ Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      <h1>Hello Project1</h1>
-    </>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
 
