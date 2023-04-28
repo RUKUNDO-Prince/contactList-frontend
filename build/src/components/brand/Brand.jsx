da93241d64b0141867871ae36cbb52a5f56b6d9c
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-04-28 22:42:24 +0300

    React

--- build/src/components/brand/Brand.jsx
+++ build/src/components/brand/Brand.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './brand.css'
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import atlassian from '../../assets/atlassian.png'
+import dropbox from '../../assets/dropbox.png'
+import shopify from '../../assets/shopify.png'
 
 const Brand = () => {
   return (
-    <div>
-        Brand 
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={ google } alt="google" />
+      </div>
+      <div>
+        <img src={ slack } alt="slack" />
+      </div>
+      <div>
+        <img src={ atlassian } alt="atlassian" />
+      </div>
+      <div>
+        <img src={ dropbox } alt="dropbox" />
+      </div>
+      <div>
+        <img src={ shopify } alt="shopify" />
+      </div>
     </div>
   )
 }
--- build/src/containers/whatGPT3/WhatGPT3.jsx
+++ build/src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './whatGPT3.css'
+import { Feature } from '../../components'
 
 const WhatGPT3 = () => {
   return (
-    <div>
-        WhatGPT3
+    <div className='gpt3__whatgpt3 section__margin' id='whgpt3'>
+        <div className="gpt3__whatgpt3-feature">
+          <Feature title={"What is GPT-3"} text={"We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." }/>
+        </div>
+        <div className="gpt3__whatgpt3-heading">
+          <h1 className="gradient__text">
+            The Possibilities Are Beyond Your Imaginations
+          </h1>
+          <p>Explore The Library</p>
+        </div>
+        <div className="gpt3__whatgpt3-container">
+          <Feature title={"Chatbots"} text={"We so opinion friends me message as delight. Whole front do of plate heard oh ought."} />
+          <Feature title={"Knowledgebase"} text={"At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" }/>
+          <Feature title={"Education"} text={"At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"} />
+        </div>
     </div>
   )
 }
